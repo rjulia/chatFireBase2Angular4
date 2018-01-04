8ffa2de85407ac39e1fad60057d1d2a87eef708c
nitroclik <reimon1972@gmail.com>
2018-01-04 16:19:52 +0800

    logical and auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { environment } from '../environments/environment';
 import { ChatService } from "./providers/chat.service";
 //Components
 import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -4,17 +4,44 @@ import { Observable } from 'rxjs/Observable';
 
 import { Mensaje } from "../interface/mensaje.interface";
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Injectable()
 export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
-  public chats: Mensaje[]=[]
+  public chats: Mensaje[]=[];
+  public usuario: any = {};
 
-  constructor(private afs: AngularFirestore) {
+  constructor(private afs: AngularFirestore,
+    public afAuth: AngularFireAuth) {
+      this.afAuth.authState.subscribe(user =>{
+        console.log('estado del usuario ', user)
+        if(!user){
+          return
+        }
 
+        this.usuario.nombre = user.displayName;
+        this.usuario.uid = user.uid;
+        this.usuario.photoURL = user.photoURL
+      })
   }
 
+  login(proveedor:string) {
+
+    if(proveedor === 'google'){
+      this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }else {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+    }
+
+  }
+  logout() {
+    this.usuario = {}
+    this.afAuth.auth.signOut();
+  }
 
   cargarMensajes(){
     this.itemsCollection = this.afs.collection<Mensaje>('chats',
@@ -37,9 +64,10 @@ export class ChatService {
 
     //todo del Id
     let mensaje: Mensaje = {
-      nombre: 'Fernado',
+      nombre: this.usuario.nombre,
       mensaje: texto,
-      data: new Date().getTime()
+      data: new Date().getTime(),
+      uid:this.usuario.uid
     }
 
     return this.itemsCollection.add(mensaje);
