a9b9bb899db91c560e78239a392e82d7f3356bc6
nitroclik <reimon1972@gmail.com>
2018-01-05 08:10:38 +0800

    fix routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: "AIzaSyBkg0WpYjQuYT5ihpcn2eA8YuHoUSu7BWM",
-    authDomain: "firechat-d6bb4.firebaseapp.com",
-    databaseURL: "https://firechat-d6bb4.firebaseio.com",
-    projectId: "firechat-d6bb4",
-    storageBucket: "firechat-d6bb4.appspot.com",
-    messagingSenderId: "957133411523"
+
   }
 };
